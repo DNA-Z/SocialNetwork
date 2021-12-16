54bf9360c129d3c5ea3ea31d99a29ebd0e349e06
Inna Borodina <creator.geek@gmail.com>
2021-12-17 01:17:03 +0300

    Add Model and model configuration

--- SocialNetwork.Domain/SocialNetwork.Domain.Entities/SocialNetwork.Domain.Entities/Models/Base/PersonInfo.cs
+++ SocialNetwork.Domain/SocialNetwork.Domain.Entities/SocialNetwork.Domain.Entities/Models/Base/PersonInfo.cs
@@ -3,12 +3,18 @@ namespace SocialNetwork.Domain.Entities.Models.Base
 {
     public class PersonInfo
     {
-        public int Id { get; set; }
+        public int Id { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string NickName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string EMailAdress { get; set; }
         public int Password { get; set; }
 
+
+        PersonInfo()
+        {
+
+        }
     }
 }
