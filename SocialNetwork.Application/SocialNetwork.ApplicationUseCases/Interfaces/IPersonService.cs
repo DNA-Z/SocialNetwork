484515c5d36441467658089f8acfecbc8be5daf4
Inna Borodina <creator.geek@gmail.com>
2021-12-28 23:52:35 +0300

    Slight changes

---
+++ SocialNetwork.Application/SocialNetwork.ApplicationUseCases/Interfaces/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using SocialNetwork.ApplicationUseCases.ViewModels;
+
+namespace SocialNetwork.ApplicationUseCases.Interfaces
+{
+    public interface IPersonService
+    {
+        /// <summary>
+        /// Метод, возвращающий коллекцию Person
+        /// </summary>
+        /// <returns></returns>
+        PersonViewModel GetPerson();
+    }
+}
---
+++ SocialNetwork.Application/SocialNetwork.ApplicationUseCases/Services/PersonService.cs
@@ -0,0 +1,18 @@
+﻿using SocialNetwork.ApplicationUseCases.Interfaces;
+using SocialNetwork.ApplicationUseCases.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.ApplicationUseCases.Services
+{
+    public class PersonService : IPersonService
+    {
+        public PersonViewModel GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
